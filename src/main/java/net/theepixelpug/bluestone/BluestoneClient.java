1f634edbff3c9df87cc75869d90f4f3a22e8a1dc
theePixelPug <schunoa@gmail.com>
2022-10-30 15:01:39 -0500

    Setup Fabric Workspace

---
+++ src/main/java/net/theepixelpug/bluestone/BluestoneClient.java
@@ -0,0 +1,9 @@
+package net.theepixelpug.bluestone;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BluestoneClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/net/theepixelpug/bluestone/BluestoneMod.java
@@ -0,0 +1,14 @@
+package net.theepixelpug.bluestone;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BluestoneMod implements ModInitializer {
+	public static final String MOD_ID = "bluestone";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
